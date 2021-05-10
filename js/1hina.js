5a102914311f251de8a946f811e0435a2314ba42
hirokeyy <82126410+hirokeyy@users.noreply.github.com>
2021-05-10 19:00:44 +0900

    Add files via upload

---
+++ js/1hina.js
@@ -0,0 +1,35 @@
+//SVGアニメーションの描画
+var stroke;
+stroke = new Vivus('mask', {//アニメーションをするIDの指定
+    start:'manual',//自動再生をせずスタートをマニュアルに
+    type: 'scenario-sync',// アニメーションのタイプを設定
+    duration: 10,//アニメーションの時間設定。数字が小さくなるほど速い
+    forceRender: false,//パスが更新された場合に再レンダリングさせない
+    animTimingFunction:Vivus.EASE,//動きの加速減速設定
+},
+function(){
+         $("#mask").attr("class", "done");//描画が終わったらdoneというクラスを追加
+}
+);
+
+$(window).on('load',function(){
+    stroke.play();//SVGアニメーションの実行
+    
+    $("#splash_logo").delay(1500).fadeOut('slow');//ロゴを1.5秒（1500ms）待機してからフェードアウト
+  
+    //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+    $("#splash").delay(1500).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+    
+        $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+  
+    });
+    //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+    
+   //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
+    $('.splashbg').on('animationend', function() {    
+        //この中に動かしたいJSを記載
+    });
+    
+});
+    
+        
